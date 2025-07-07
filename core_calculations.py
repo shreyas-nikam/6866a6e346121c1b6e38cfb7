fc54df486b4768327424a6fa5960c91101c00aca
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-07-07 10:48:09 -0400

    Add core_calculations.py

---
+++ core_calculations.py
@@ -0,0 +1,8 @@
+
+def calculate_bilateral_links(num_intermediaries):
+    # Calculates the number of bilateral links
+    return num_intermediaries * (num_intermediaries - 1) // 2
+
+def calculate_ccp_links(num_intermediaries):
+    # Calculates the number of CCP links
+    return num_intermediaries
