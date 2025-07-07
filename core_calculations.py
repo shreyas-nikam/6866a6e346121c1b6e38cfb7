f6814171e952bd0d358ddf83c4ad8f3cc5c2f5a2
rishabhqu <rishabh@qusandbox.com>
2025-07-07 10:40:56 -0400

    Reset repository to starter files

--- core_calculations.py
+++
@@ -1,8 +0,0 @@
-
-def calculate_bilateral_links(num_intermediaries):
-    """Calculates the number of bilateral links in a network."""
-    return num_intermediaries * (num_intermediaries - 1) // 2
-
-def calculate_ccp_links(num_intermediaries):
-    """Calculates the number of links to a CCP."""
-    return num_intermediaries
